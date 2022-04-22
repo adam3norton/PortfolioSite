ea975e71c2b48e02b206d57da9139808ea186362
Adam Norton <adamnorton@Adams-MacBook-Pro.local>
2022-04-22 17:50:17 -0600

    Header, Footer, Landing,  and Passions

--- PortfolioSite/src/app/app.module.ts
+++ PortfolioSite/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PassionsComponent } from './passions/passions.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    PassionsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
